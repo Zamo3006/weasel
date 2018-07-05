7d34575d5f73e28bba175ff2f00e7ecf7d2715e0
Robin Fischer <robin@rf-fischer.com>
2018-07-05 13:51:05 +0000

    Methode für Tabwechsel hinzugefügt

--- webapp/controller/CoreView.controller.js
+++ webapp/controller/CoreView.controller.js
@@ -9,10 +9,24 @@ sap.ui.define([
 	"use strict";
 
 	var IconTabBarController = Controller.extend("sap.m.sample.IconTabBarProcess.IconTabBar", {
-
+	
+	loadButtonColors: function(oEvent){
+		
+		var key = oEvent.getParameter("key");
+		console.log(key);
+		if (key == "Beladen"){
+			
+		}
+		else{
+			if(key =="Entladen"){
+				
+			}
+		}
+	}
+	
 	});
 	
- 
+	
 	return IconTabBarController;
  
 });
